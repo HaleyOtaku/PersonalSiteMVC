6c8e3ea5d7e4436f54794b1297f6fe4f14d6cc8f
HaleyOtaku <haleyotaku@gmail.com>
2024-03-21 16:57:52 -0400

    Updated Contact Form

---
+++ PersonalSiteMVC.UI/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalSiteMVC.UI.Models;
+using System.Diagnostics;
+using Microsoft.Extensions.Options;//Grants access to IOptions<T>
+using MimeKit; //Added for success
+using MailKit.Net.Smtp;//Added for access to Smtp Client class
+
+namespace PersonalSiteMVC.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly CredentialSettings _credentials;
+        private readonly IConfiguration _config;
+        public HomeController(IConfiguration config) { _config = config; }
+        public HomeController(ILogger<HomeController> logger, IOptions<CredentialSettings> credentials)
+        {
+            _logger = logger;
+            _credentials = credentials.Value;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult Resume()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Contact(ContactViewModel cvm)
+        {
+            string message = $"You have received a new email from your site's contact form!<br/>" +
+                $"Sender: {cvm.Name}<br />" +
+                $"Email: {cvm.Email}<br />" +
+                $"Subject: {cvm.Subject}<br />" +
+                $"Message: {cvm.Message}";
+
+            var mm = new MimeMessage();
+  
+            mm.From.Add(new MailboxAddress("Sender", _credentials.Email.Username));      
+            mm.To.Add(new MailboxAddress("Personal", _credentials.Email.Recipient));
+            mm.Subject = $"New contact form message: [{cvm.Subject}]";        
+            mm.Body = new TextPart("HTML") { Text = message };  
+            mm.Priority = MessagePriority.Urgent;
+            mm.ReplyTo.Add(new MailboxAddress("User", cvm.Email));
+    
+            using (var client = new SmtpClient())
+            {            
+                client.Connect(_credentials.Email.Server, 8889);        
+                client.Authenticate(_credentials.Email.Username, _credentials.Email.Password);
+                try
+                {
+                   
+                    client.Send(mm);
+                }
+                catch (Exception ex)
+                {
+                   
+                    ViewBag.ErrorMessage = $"There was an error in processing your request.\n"
+                        + $"Please try again later.<br /> Error Message: {ex.StackTrace}";
+
+                  
+                    return View(cvm);
+                }
+            }
+            return View("EmailConfirmation", cvm);
+        }
+    }
+}
---
+++ PersonalSiteMVC.UI/Models/ContactViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;//added for access to annotations for validation
+
+namespace PersonalSiteMVC.UI.Models
+{
+    public class ContactViewModel
+    {
+        //We can use Data Annotations to add validation to our model.
+        //This is useful when we have required fields or need data with certain formatting,
+        //as it allows us to verify those conditions are met and display messages
+        //in the front end to let the user know what to do:
+
+        //Annotations ALWAYS go above + space from prop they apply to
+
+        [Required(ErrorMessage = "*Name is required")]//Makes the field required 
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "*Email is required")]
+
+        [DataType(DataType.EmailAddress)]//This ensures minimal email formatting (chars + @ + chars + . + chars)
+
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "*Subject is required")]
+
+        public string Subject { get; set; } = null!;
+
+        [Required(ErrorMessage = "*Message is required")]
+        [DataType(DataType.MultilineText)]//This ensures the textbox will be larger than an input tag
+        public string Message { get; set; } = null!;
+
+    }
+}
